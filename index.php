5c43f98495069eff2d32e51f957a5678109f7fe6
Charlie Dennnington <charlie.dennington@u-dox.com>
2013-02-13 17:57:11 +0000

    second question complete and third started

--- index.php
+++ index.php
@@ -8,14 +8,47 @@
 <body>
     <h1>Question 1</h1>
     <?php
-    $num;
+    $num_q1;
         for ($i=0; $i<=999; $i++)
         {
             if ($i%3 == 0 OR $i%5 == 0) {
-                $num = $i + $num;
+                $num_q1 = $i + $num_q1;
             }
         }
-        echo "last nunber " . $num . "<br>";
+        echo "last nunber " . $num_q1 . "<br>";
+    ?>
+    <h1>Question 2</h1>
+    <?php
+    $num_one = 0;
+    $num_two = 1;
+    $q2_answer=0;
+
+    while($z < 4000000) {
+
+        $z = $num_one + $num_two; 
+        $num_one = $num_two;
+        $num_two = $z;
+        
+        if ($z%2 == 0) {
+            #echo($z."<br />");
+            $q2_answer = $z + $q2_answer;
+        }
+    }
+    echo $q2_answer;
+
+    ?>
+    <h1>Question 3</h1>
+    <?php
+    $sq_root = round(sqrt(600851475143));
+    echo $sq_root;
+
+    for ($i=1; $i<$sq_root; $i++) {
+        if ($i%2 == 0) {
+            echo($i."<br />");
+        }
+    }
+    
+
     ?>
 </body>
 </html>
